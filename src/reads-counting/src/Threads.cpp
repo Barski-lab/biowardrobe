77c48f2def5574fbe51fd70a18fb10e37a72e1b7
Andrey Kartashov <porter@P12-0709.CHMCCORP>
2012-12-19 12:36:39 -0500

    T7

--- src/reads-counting/src/Threads.cpp
+++ src/reads-counting/src/Threads.cpp
@@ -170,7 +170,7 @@ void sam_reader_thread::run(void)
                                 matrix.setElement(c,column,0.0);
                             }
                             else
-                            if((p_val=Math::Poisson_cdist<double>(tot,lambda*(double)exon_len))>0.01 )
+                            if(((exon_len<300)&&(p_val=Math::Poisson_cdist<double>(tot,lambda*(double)exon_len))>0.01) || ((exon_len>=300) && tot< 10))
                             { /*trying to ignore not relevant data*/
 
                                 matrix.setElement(c,column,matrix.getLimit());
