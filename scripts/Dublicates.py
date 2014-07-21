b07af5a69fe3c7963cf2f01f91202dc4822f458e
Andrey Kartashov <porter@porter.st>
2014-07-21 13:16:51 -0400

    Remove duplicated sequences

--- scripts/Dublicates.py
+++ scripts/Dublicates.py
@@ -22,11 +22,6 @@
 #  **
 #  ****************************************************************************/
 
-##
-## Trim fastq file from 5' (left) and 3' (right) end
-##
-##
-##
 
 import sys
 from optparse import OptionParser
@@ -34,8 +29,6 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-l", "--left", action="store", type="int",
                   dest="left", help="Trimm from left", metavar="<int>")
-parser.add_option("-r", "--right", action="store", type="int",
-                  dest="right", help="Trimm from right", metavar="<int>")
 
 (opt, args) = parser.parse_args(sys.argv)
 
@@ -43,18 +36,40 @@ left = 0
 if opt.left:
     left = int(opt.left)
 
-right = 0
-if opt.right:
-    right = int(opt.right)
-
-if left == 0 and right == 0:
+if left == 0:
     sys.exit(0)
 
 b = 0
+l1 = str()
+l2 = str()
+l3 = str()
+l4 = str()
+adap = str()
+seq = str()
+qual = str()
+
+data = {}
+
 for line in sys.stdin:
     line = line.strip()
     b += 1
-    if b % 2 == 0:
-        print line[left:len(line)-right]
-    else:
-        print line
+    if b == 1:
+        l1 = line
+    elif b == 2:
+        l2 = line
+    elif b == 3:
+        l3 = line
+    elif b == 4:
+        l4 = line
+        adap = l2[0:left]
+        seq = l2[left+1:len(l2)]
+        b = 0
+        if adap not in data:
+            data[adap] = list()
+        if seq not in data[adap]:
+            qual = l4[left+1:len(l4)]
+            data[adap].append(seq)
+            print l1
+            print seq
+            print l3
+            print qual
