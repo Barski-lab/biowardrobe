d877c8203b7bbd75ce56fc23e019a56e521b11a9
Andrey Kartashov <porter@porter.st>
2015-04-12 14:30:47 -0400

    EMS base frequency plot has save chart button

--- EMS/ems/app/view/charts/Fence.js
+++ EMS/ems/app/view/charts/Fence.js
@@ -22,118 +22,162 @@
 
 Ext.require('Ext.chart.*');
 
-Ext.define('EMS.view.charts.Fence',
-           {
-               extend: 'Ext.chart.Chart',
-               alias: 'widget.chartfence',
-               style: 'background:#fff',
-               animate: true,
-               legend: {
-                   position: 'right'
-               },
+Ext.define('EMS.view.charts.Fence', {
+    extend: 'Ext.Panel',//chart.Chart',
+    alias: 'widget.chartfence',
+    store: 'Fence',
+    layout: 'fit',
+    initComponent: function () {
+        var me = this;
 
-               store: 'Fence',
-               shadow: true,
-               theme: 'Category1',
-               axes: [
-                   {
-                       type: 'Numeric',
-                       minimum: 0,
-                       position: 'left',
-                       fields: ['A', 'C', 'T', 'G','N'],
-                       title: 'Frequency',
-                       minorTickSteps: 1,
-                       grid: true
-                   },
-                   {
-                       type: 'Category',
-                       position: 'bottom',
-                       fields: ['id'],
-                       title: 'Nucleotide position',
-                       grid: true,
-                       majorTickSteps: 10
-                   }
-               ],
-               series: [
-                   {
-                       type: 'line',
-                       highlight: {
-                           size: 7,
-                           radius: 7
-                       },
-                       axis: 'left',
-                       xField: 'id',
-                       yField: 'A',
-                       markerConfig: {
-                           type: 'circle',
-                           size: 1,
-                           radius: 1,
-                           'stroke-width': 0
-                       }
-                   },
-                   {
-                       type: 'line',
-                       highlight: {
-                           size: 7,
-                           radius: 7
-                       },
-                       axis: 'left',
-                       xField: 'id',
-                       yField: 'C',
-                       markerConfig: {
-                           type: 'circle',
-                           size: 1,
-                           radius: 1,
-                           'stroke-width': 0
-                       }
-                   },
-                   {
-                       type: 'line',
-                       highlight: {
-                           size: 7,
-                           radius: 7
-                       },
-                       axis: 'left',
-                       xField: 'id',
-                       yField: 'T',
-                       markerConfig: {
-                           type: 'circle',
-                           size: 1,
-                           radius: 1,
-                           'stroke-width': 0
-                       }
-                   },
-                   {
-                       type: 'line',
-                       highlight: {
-                           size: 7,
-                           radius: 7
-                       },
-                       axis: 'left',
-                       xField: 'id',
-                       yField: 'G',
-                       markerConfig: {
-                           type: 'circle',
-                           size: 1,
-                           radius: 1,
-                           'stroke-width': 0
-                       }
-                   },
-                   {
-                       type: 'line',
-                       highlight: {
-                           size: 7,
-                           radius: 7
-                       },
-                       axis: 'left',
-                       xField: 'id',
-                       yField: 'N',
-                       markerConfig: {
-                           type: 'circle',
-                           size: 1,
-                           radius: 1,
-                           'stroke-width': 0
-                       }
-                   }
-               ]
-           });
+        this.chart = Ext.create('Ext.chart.Chart', {
+                                    style: 'background:#fff',
+                                    animate: true,
+                                    legend: {
+                                        position: 'right'
+                                    },
+                                    store: me.initialConfig.store,
+                                    shadow: true,
+                                    theme: 'Category1',
+                                    axes: [
+                                        {
+                                            type: 'Numeric',
+                                            minimum: 0,
+                                            position: 'left',
+                                            fields: ['A', 'C', 'T', 'G', 'N'],
+                                            title: 'Frequency',
+                                            minorTickSteps: 1,
+                                            grid: true
+                                        },
+                                        {
+                                            type: 'Category',
+                                            position: 'bottom',
+                                            fields: ['id'],
+                                            title: 'Nucleotide position',
+                                            grid: true,
+                                            majorTickSteps: 10
+                                        }
+                                    ],
+                                    series: [
+                                        {
+                                            type: 'line',
+                                            highlight: {
+                                                size: 7,
+                                                radius: 7
+                                            },
+                                            axis: 'left',
+                                            xField: 'id',
+                                            yField: 'A',
+                                            markerConfig: {
+                                                type: 'circle',
+                                                size: 1,
+                                                radius: 1,
+                                                'stroke-width': 0
+                                            }
+                                        },
+                                        {
+                                            type: 'line',
+                                            highlight: {
+                                                size: 7,
+                                                radius: 7
+                                            },
+                                            axis: 'left',
+                                            xField: 'id',
+                                            yField: 'C',
+                                            markerConfig: {
+                                                type: 'circle',
+                                                size: 1,
+                                                radius: 1,
+                                                'stroke-width': 0
+                                            }
+                                        },
+                                        {
+                                            type: 'line',
+                                            highlight: {
+                                                size: 7,
+                                                radius: 7
+                                            },
+                                            axis: 'left',
+                                            xField: 'id',
+                                            yField: 'T',
+                                            markerConfig: {
+                                                type: 'circle',
+                                                size: 1,
+                                                radius: 1,
+                                                'stroke-width': 0
+                                            }
+                                        },
+                                        {
+                                            type: 'line',
+                                            highlight: {
+                                                size: 7,
+                                                radius: 7
+                                            },
+                                            axis: 'left',
+                                            xField: 'id',
+                                            yField: 'G',
+                                            markerConfig: {
+                                                type: 'circle',
+                                                size: 1,
+                                                radius: 1,
+                                                'stroke-width': 0
+                                            }
+                                        },
+                                        {
+                                            type: 'line',
+                                            highlight: {
+                                                size: 7,
+                                                radius: 7
+                                            },
+                                            axis: 'left',
+                                            xField: 'id',
+                                            yField: 'N',
+                                            markerConfig: {
+                                                type: 'circle',
+                                                size: 1,
+                                                radius: 1,
+                                                'stroke-width': 0
+                                            }
+                                        }
+                                    ]
+                                }
+        );
+
+
+        this.tbar = [{
+                         xtype: 'fieldcontainer',
+                         layout: 'hbox',
+                         items: [
+                             {
+                                 xtype: 'button',
+                                 text: 'Save Chart',
+                                 iconCls: 'svg-logo',
+                                 handler: function () {
+                                     var p = Ext.create('Ext.form.Panel', {
+                                         standardSubmit: true,
+                                         url: 'data/svg.php',
+                                         hidden: true,
+                                         items: [
+                                             //{xtype: 'hiddenfield', name: 'id', value: me.initialConfig.plotTitle},
+                                             {xtype: 'hiddenfield', name: 'type', value: "image/svg+xml"},
+                                             {xtype: 'hiddenfield', name: 'svg', value: me.chart.save()}
+                                         ]
+                                     });
+                                     p.getForm().submit
+                                     ({
+                                          success: function (form, action) {
+                                              p.destroy();
+                                          }
+                                      });
+                                 }
+                             }
+                         ]
+                     }];
+        me.items = me.chart;
+        me.callParent(arguments);
+    },
+    onDestroy: function () {
+        this.chart.destroy();
+        this.callParent(arguments);
+    },
+});
